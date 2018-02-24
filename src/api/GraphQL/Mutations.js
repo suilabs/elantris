9118ad98e0314098e39edac6b46d6b0ae4ee38a8
Borja Arias <borja.arias.upc@gmail.com>
2018-02-24 18:32:41 +0100

    Fixed linting

--- src/api/GraphQL/Mutations.js
+++ src/api/GraphQL/Mutations.js
@@ -1,13 +1,13 @@
 
 const Mutations = {
-  insertProject: "mutation insertProject($project: ProjectInput!) { insertProject(project: $project) { key title }}",
+  insertProject: 'mutation insertProject($project: ProjectInput!) { insertProject(project: $project) { key title }}',
   insertType: `
   mutation insertType($type: String!){
     insertProjectType(type: $type) {
       key
       name
     }
-  }`
+  }`,
 };
 
 export default Mutations;
