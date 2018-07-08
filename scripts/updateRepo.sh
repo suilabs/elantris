4876f593fc9f2c85c53c7f2661568ee3ca869fcb
ShakMR <borja.arias.upc@gmail.com>
2018-07-08 17:20:11 +0200

    Fix docker instance running in foreground

--- scripts/updateRepo.sh
+++ scripts/updateRepo.sh
@@ -4,4 +4,4 @@ ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "
 cd Projects/suilabs
 git fetch && \
 git pull origin master && \
-docker-compose up --build frontend"
+docker-compose up -d --build frontend"
