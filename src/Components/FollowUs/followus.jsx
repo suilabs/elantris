533f5176d8eb21e313fa3ad1dbd95baf89e4af5e
Borja Arias <borja.arias.upc@gmail.com>
2018-11-15 17:17:59 +0100

    SOFT-7 follow us moved to footer

--- src/Components/FollowUs/followus.jsx
+++ src/Components/FollowUs/followus.jsx
@@ -4,24 +4,26 @@ import PropTypes from 'prop-types';
 import './followus.scss';
 
 const FollowUs = (props) => {
-  if (!props.show) {
-    return null;
-  }
   const classNames = `sui-follow-us ${props.className}`;
   return (
     <div className={classNames}>
       <p>
         follow us:
       </p>
-      <a href="#linkedin" className="menu-item sui-follow__item">LinkedIn</a>
-      <a href="#behance" className="menu-item sui-follow__item">Behance</a>
-      <a href="#instagram" className="menu-item sui-follow__item">Instagram</a>
+      <a href="#linkedin" className="menu-item sui-follow__item">
+        <img src="/icons/ln.png" alt="LinkedIn" />
+      </a>
+      <a href="#behance" className="menu-item sui-follow__item">
+        <img src="/icons/ln.png" alt="Behance" />
+      </a>
+      <a href="#instagram" className="menu-item sui-follow__item">
+        <img src="/icons/ln.png" alt="Instagram" />
+      </a>
     </div>
   );
 };
 
 FollowUs.propTypes = {
-  show: PropTypes.bool.isRequired,
   className: PropTypes.string,
 };
 
--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import FollowUs from '../FollowUs';
-import { VFlexBox, justification } from '../Common/FlexBoxes';
+import FollowUs from '../FollowUs/followus';
 
 import './footer.scss';
 
@@ -13,14 +11,18 @@ const Footer = (props) => {
   return (
     <div id="sui-footer" className="sui-footer">
       <div className="sui-footer__contact">
-        <VFlexBox justify={justification.start} vAlign={justification.start}>
-          <span className="sui-footer__consulta">tens una consulta?</span>
-          <span className="sui-footer__telephone">+34 646 24 85 27</span>
-          <a className="sui-footer__mail" href="mailto://anna@suilabs.com">anna at suilabs.com</a>
-          <a className="sui-footer__mail" href="mailto://borja@suilabs.com">borja at suilabs.com</a>
-        </VFlexBox>
+        <p className="sui-footer__consulta">tens una<br />consulta?</p>
+        <p className="sui-footer__telephone">
+          <a href="tel:+34646248527"> +34 646 24 85 27 </a>
+        </p>
+        <p className="sui-footer__mail">
+          <a href="mailto:anna@suilabs.com">anna@suilabs.com</a>
+        </p>
+        <p className="sui-footer__mail">
+          <a href="mailto:borja@suilabs.com">borja@suilabs.com</a>
+        </p>
       </div>
-      <FollowUs className="sui-footer__social" show />
+      <FollowUs />
     </div>
   );
 };
