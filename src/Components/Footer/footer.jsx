5860846bdfb66e1d6ef2aad19a95e0ff2aa9c67e
Borja Arias <borjaarias@borjaria03m.suilabs>
2019-06-10 22:41:24 +0200

    SOFT-XX Use cloudfrount urls for static files. offuscate emails

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -6,6 +6,27 @@ import translations from '../../Services/TranslationService';
 
 import './footer.scss';
 
+const emails = [
+  'moc.sbalius@anna',
+  'moc.sbalius@ajrob',
+];
+
+class MailLink extends React.Component {
+  setRef = email => (ref) => {
+    ref.setAttribute('href', `mailto:${email.split('').reverse().join('')}`);
+  };
+
+  render() {
+    // eslint-disable-next-line jsx-a11y/anchor-is-valid
+    return emails.map(email => (
+      <p className="sui-footer__mail">
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a ref={this.setRef(email)}>{email}</a>
+      </p>
+    ));
+  }
+}
+
 const Footer = (props) => {
   if (props.nonVisible) {
     return '';
@@ -23,12 +44,7 @@ const Footer = (props) => {
         <p className="sui-footer__telephone">
           <a href="tel:+34646248527"> +34 646 24 85 27 </a>
         </p>
-        <p className="sui-footer__mail">
-          <a href="mailto:anna@suilabs.com">anna@suilabs.com</a>
-        </p>
-        <p className="sui-footer__mail">
-          <a href="mailto:borja@suilabs.com">borja@suilabs.com</a>
-        </p>
+        <MailLink />
       </div>
       <FollowUs />
     </div>
