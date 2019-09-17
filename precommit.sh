fb7569d9612b52e8ee4a1eb63cc3c2271134056c
ShakMR <borja.arias.upc@gmail.com>
2019-09-17 23:55:22 +0200

    Pages are now loaded using a fade-in. Email is no longer an anchor but an input that copies the email to your clipboard. Projects cover are now 100% on mobile

--- precommit.sh
+++ precommit.sh
@@ -22,10 +22,11 @@ main () {
     echo -n "No SCSS files to check -> ";
   else
     echo -n "Checking $cssLen SCSS Files for linting errors..."
-    ./node_modules/.bin/stylelint ${cssFilesToCommit[@]} > /tmp/csslint 2>/dev/null
+    ./node_modules/.bin/stylelint ${cssFilesToCommit[@]} --fix > /tmp/csslint 2>/dev/null
   fi
 
   if [[ $? -eq 0 ]]; then
+    git add -u
     echo -e "${GREEN}✔${NC}"
   else
     echo -e "${RED}✘${NC}"
@@ -42,10 +43,11 @@ main () {
   if [[ $jsLen -eq 0 ]]; then
     echo -n "No JS files to check -> "; else
     echo -n "Checking $jsLen JS Files for linting errors..."
-    ./node_modules/.bin/eslint ${jsFilesToCommit[@]} > /tmp/jslint 2>/tmp/jslintError
+    ./node_modules/.bin/eslint ${jsFilesToCommit[@]} --fix > /tmp/jslint 2>/tmp/jslintError
   fi
 
   if [[ $? -eq 0 ]]; then
+    git add -u
     echo -e "${GREEN}✔${NC}"
   else
     echo -e "${RED}✘${NC}"
