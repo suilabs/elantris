30eabf352e04fdcd975edc1a391bf9fe1f9b8a8e
Borja Arias <borja.arias.upc@gmail.com>
2020-11-13 21:08:25 +0100

    Change readme and remove storybook

--- stories/Components/Common/spinner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import Spinner from '../../../src/Components/Common/Loading/Spinner';
-
-storiesOf('Spinner', module)
-  .add('is Animated', () => (
-    <Spinner />
-  ));
--- stories/Components/icons.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import { MenuIcon } from '../../src/Components/Icons';
-
-storiesOf('Icons', module)
-  .add('Menu', () => (
-    <MenuIcon />
-  ));
