b337e85ff2ac30feda5eca740a4819b07b757285
ShakMR <borja.arias.upc@gmail.com>
2022-01-24 10:33:42 +0100

    Check if scrollTo exists. This is giving problems in server side rendering

--- src/Views/projects.jsx
+++ src/Views/projects.jsx
@@ -27,7 +27,7 @@ class Projects extends React.Component {
       <ProjectList
         section={section}
         onItemClick={(event, projectName) => {
-          window.scrollTo(0, 0);
+          if (window.scrollTo) window.scrollTo(0, 0);
           eye.seeClick('Project', 'Open', projectName);
         }}
         path={match.params.project}
